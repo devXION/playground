0bbbdc9260dc98694cfd487c598af85a98a9b015
XION <83879915+devXION@users.noreply.github.com>
2022-07-26 12:19:49 +0200

    Add files via upload

--- spotimain.py
+++ spotimain.py
@@ -6,24 +6,33 @@ from spotipy.oauth2 import SpotifyClientCredentials
 @click.command()
 @click.option('--cid', prompt='your cid')
 @click.option('--secret', prompt='your secret')
-def credits(cid, secret):
+@click.option('--playlink', prompt='playlist link')
+@click.option('--stat', prompt='stat to display')
+def credits(cid, secret, playlink,stat):
     click.echo(f'credentials have been entered')
-    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    playlist_link = 'https://open.spotify.com/playlist/2M2ERqBl2vRKEV1Hc23Poc?si=0535071291624040'
+    client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client)
+    playlist_link = playlink
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-    track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
     for track in sp.playlist_tracks(playlist_URI)["items"]:
-        track_uri = track["track"]["uri"]
+        [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
         track_name = track["track"]["name"]
-        artist_uri = track["track"]["artists"][0]["uri"]
-        artist_info = sp.artist(artist_uri)
         artist_name = track["track"]["artists"][0]["name"]
-        artist_pop = artist_info["popularity"]
-        artist_genres = artist_info["genres"]
         album = track["track"]["album"]["name"]
-        track_pop = track["track"]["popularity"]
-        print(track_name)
+        if stat == 'track name':
+            print(track_name)
+        if stat == 'artistname':
+            print(artist_name)
+        if stat == 'album':
+            print(album)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     credits()
