1278941719b41cb2c5712372e0e89656d23a96a9
XION <83879915+devXION@users.noreply.github.com>
2022-07-27 12:01:58 +0200

    Add files via upload

--- spotimain.py
+++ spotimain.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import click
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -7,13 +9,16 @@ from spotipy.oauth2 import SpotifyClientCredentials
 @click.option('--cid', prompt='your cid')
 @click.option('--secret', prompt='your secret')
 @click.option('--playlink', prompt='playlist link')
-@click.option('--stat', prompt='stat to display')
-def credits(cid, secret, playlink,stat):
+@click.option('--stat', '-t', type=click.Choice(['trackname', 'artistname', 'album']), multiple=True)
+def credits(cid, secret, playlink, stat):
+    time.sleep(2)
     click.echo(f'credentials have been entered')
     client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client)
-    playlist_link = playlink
-    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+    playlist_URI = playlink.split("/")[-1].split("?")[0]
+    time.sleep(1)
+    print('statistics to highlight')
+    stat = input('')
     for track in sp.playlist_tracks(playlist_URI)["items"]:
         [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
         track_name = track["track"]["name"]
@@ -21,17 +26,10 @@ def credits(cid, secret, playlink,stat):
         album = track["track"]["album"]["name"]
         if stat == 'track name':
             print(track_name)
-        if stat == 'artistname':
+        if stat == 'artist name':
             print(artist_name)
         if stat == 'album':
             print(album)
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
