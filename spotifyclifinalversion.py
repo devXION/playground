1cf2876cc1e3e0f1e041c7a38e7f0e32500f977a
XION <83879915+devXION@users.noreply.github.com>
2022-07-28 17:59:43 +0200

    SpotifyCLI Final Rework
    Changelog:
    - Variables assigned to elements in list have been removed and now they are directly used in **if input ==** segment.

---
+++ spotifyclifinalversion.py
@@ -0,0 +1,26 @@
+import click
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+@click.command()
+@click.password_option('--cid', prompt='your cid', hide_input=False, confirmation_prompt=False)
+@click.password_option('--secret', prompt='your secret', hide_input=False, confirmation_prompt=False)
+@click.password_option('--playlink', prompt='playlist link', required=True,hide_input=False, confirmation_prompt=False )
+@click.option('--stat', type=click.Choice(['track name', 'artist name', 'album']), prompt='')
+def credits(cid, secret, playlink, stat):
+    click.echo(f'credentials have been entered')
+    client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager=client)
+    playlist_URI = playlink.split("/")[-1].split("?")[0]
+    for track in sp.playlist_tracks(playlist_URI)["items"]:
+        [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+        if stat == 'track name':
+            print(track["track"]["name"])
+        if stat == 'artist name':
+            print(track["track"]["artists"][0]["name"])
+        if stat == 'album':
+            print(track["track"]["album"]["name"])
+
+if __name__ == '__main__':
+    credits()
