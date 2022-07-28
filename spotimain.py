3b6bc99119fd745248517810ddf21f123653dbf4
XION <83879915+devXION@users.noreply.github.com>
2022-07-28 12:37:53 +0200

    Spotify CLI update
    What has been updated:
    - CID and SECRET is now  changed from @click.option to @click.password.option.
    - user input is required during CID/SECRET/PLAYLINK functions, without it code will not execute properly asking user to input data.
    - time module has been removed due to being not needed and excessive.
    - optimized compute speed 
    - stat argument in click.option('--stat') now works properly. Reassigning had been removed.
    - Small changes to click.Choice. Now user can see available options and if input is not matching given options, message appears saying there is no such stat  and prompts user input once again until it meets requirements.

--- spotimain.py
+++ spotimain.py
@@ -1,23 +1,18 @@
-import time
 import click
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
 @click.command()
-@click.option('--cid', prompt='your cid')
-@click.option('--secret', prompt='your secret')
-@click.option('--playlink', prompt='playlist link')
-@click.option('--stat', '-t', type=click.Choice(['trackname', 'artistname', 'album']), multiple=True)
+@click.password_option('--cid', prompt='your cid', hide_input=False, confirmation_prompt=False)
+@click.password_option('--secret', prompt='your secret', hide_input=False, confirmation_prompt=False)
+@click.password_option('--playlink', prompt='playlist link', required=True,hide_input=False, confirmation_prompt=False )
+@click.option('--stat', type=click.Choice(['track name', 'artist name', 'album']), prompt='')
 def credits(cid, secret, playlink, stat):
-    time.sleep(2)
     click.echo(f'credentials have been entered')
     client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client)
     playlist_URI = playlink.split("/")[-1].split("?")[0]
-    time.sleep(1)
-    print('statistics to highlight')
-    stat = input('')
     for track in sp.playlist_tracks(playlist_URI)["items"]:
         [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
         track_name = track["track"]["name"]
@@ -29,6 +24,7 @@ def credits(cid, secret, playlink, stat):
             print(artist_name)
         if stat == 'album':
             print(album)
+
 
 
 if __name__ == '__main__':
