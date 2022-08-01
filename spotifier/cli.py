d02c16b3af3c6d1ba197bd6da76c6e468f542e85
XION <83879915+devXION@users.noreply.github.com>
2022-08-01 13:41:11 +0200

    CLI PEP8 reformat (#8)
    * CLI PEP8 reformat 
    
    Code is now compatible with PEP8 styleguide
    
    Co-authored-by: xion <jstachecki@GDN-N-JOZEFS.advaoptical.com>

--- spotifier/cli.py
+++ spotifier/cli.py
@@ -1,23 +1,45 @@
+"""CLI for function calls."""
 import click
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
 @click.command()
-@click.password_option('--cid', prompt='your cid', hide_input=False, confirmation_prompt=False)
-@click.password_option('--secret', prompt='your secret', hide_input=False, confirmation_prompt=False)
-@click.password_option('--playlink', prompt='playlist link', required=True,hide_input=False, confirmation_prompt=False )
-@click.option('--stat', type=click.Choice(['track name', 'artist name', 'album']), prompt='')
+@click.password_option(
+    "--cid",
+    prompt="your cid",
+    hide_input=False,
+    confirmation_prompt=False
+)
+@click.password_option(
+    "--secret",
+    prompt="your secret",
+    hide_input=False,
+    confirmation_prompt=False
+)
+@click.password_option(
+    "--playlink",
+    prompt="playlist link",
+    required=True,
+    hide_input=False,
+    confirmation_prompt=False,
+)
+@click.option(
+    "--stat",
+    type=click.Choice(["track name", "artist name", "album"]),
+    prompt=""
+)
 def cli(cid, secret, playlink, stat):
-    click.echo(f'credentials have been entered')
+    """CLI defined as function."""
+    click.echo(f"credentials have been entered")
     client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client)
     playlist_URI = playlink.split("/")[-1].split("?")[0]
-    for track in sp.playlist_tracks(playlist_URI)["items"]:
-        if stat == 'track name':
-            print(track["track"]["name"])
-        if stat == 'artist name':
-            print(track["track"]["artists"][0]["name"])
-        if stat == 'album':
-            print(track["track"]["album"]["name"])
 
+    for track in sp.playlist_tracks(playlist_URI)["items"]:
+        if stat == "track name":
+            print(track["track"]["name"])
+        if stat == "artist name":
+            print(track["track"]["artists"][0]["name"])
+        if stat == "album":
+            print(track["track"]["album"]["name"])
