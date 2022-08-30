8b3c93615e030e1e712b6d4e5512a00b56253240
devXION <xionroxard@gmail.com>
2022-08-30 14:59:49 +0200

    updated version of cli

--- INTCli.py
+++ INTCli.py
@@ -1,6 +1,7 @@
 import click
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 print('Welcome to spotifier ver 1.0, This project will help you find Spotify song'
       'suggestions! Please do the following below:')
@@ -9,20 +10,26 @@ print('Welcome to spotifier ver 1.0, This project will help you find Spotify son
 @click.command()
 @click.option(
     "--cid",
-    # prompt="Enter your cid",
-    # hide_input=False,
-    # confirmation_prompt=False,
+    prompt="Enter your cid",
+    hide_input=False,
+    confirmation_prompt=False,
+    envvar="SPOTIFY_CID",
     help='CID can be found at Spotify Dashboard'
 )
 @click.option(
     "--secret",
-    # prompt="Enter your secret",
-    # hide_input=False,
-    # confirmation_prompt=False,
+    prompt="Enter your secret",
+    hide_input=False,
+    confirmation_prompt=False,
+    envvar="SPOTIFY_SECRET",
     help='Secret can be found at Spotify Dashboard'
 )
 def cli(cid, secret):
-    sp = spotify_login(cid, secret)
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
+                                                   client_secret=secret,
+                                                   redirect_uri='http://example.com/callback/',
+                                                   scope='user-library-read'))
+
     click.echo("This command will guide you through finding your songs recommendation.")
     seeds_artists = []
     seeds_tracks = []
@@ -34,14 +41,16 @@ def cli(cid, secret):
           - A artist name (Metalica)
           - A artist URL (Example here)
           - A artist Spotify ID (EXAMPLE HERE)"""
-        )
+                   )
 
         artist = click.prompt("Search for an artist (or leave blank to continue)", default="", type=str)
 
         while artist:
             result = get_artist(sp, artist)
-            seeds_artists.append(result)
-            artist = click.prompt("Search for an artist", default="", type=str)
+            for i in result['artists']['items']:
+                seeds_artists.append(i['name'])
+                artist = click.prompt("Search for an artist", default="", type=str)
+                break
 
     if click.confirm("Would you like to define track seeds?"):
         click.echo("""\
@@ -49,19 +58,23 @@ def cli(cid, secret):
           - A track name (Careless whisper)
           - A track URL (Example here)
           - A track Spotify ID (EXAMPLE HERE)"""
-        )
+                   )
 
         track = click.prompt("Search for an track (or leave blank to continue)", default="", type=str)
 
         while track:
             result = get_track(sp, track)
-            seeds_tracks.append(result)
-            track = click.prompt("Search for a track", default="", type=str)
+            for i in result['tracks']['items']:
+                seeds_tracks.append(i['name'])
+                track = click.prompt("Search for a track", default="", type=str)
+                break
 
     if click.confirm("Would you like to define genre seeds?"):
         available_genres = sp.recommendation_genre_seeds()["genres"]
         genres_numered = [f"[{i}] {x}" for i, x in enumerate(available_genres)]
-        click.echo(f"Available genres:\n{genres_numered}")
+        click.echo(f"Available genres:")
+        for entry in genres_numered:
+            click.echo(f"{entry}")
 
         genre = click.prompt("Enter genre # to add:", default="", type=str)
 
@@ -77,29 +90,30 @@ def cli(cid, secret):
     click.echo("Seeds genres:")
     click.echo(seeds_genres)
     click.echo()
-    recommendations = get_recommendations(sp, seeds_artists, seeds_tracks, seeds_genres)
-    print("DO SOMETHING WITH RECOMMENDATIONS")
-
-
-def is_url(url):
-    return True
-
-
-def spotify_login(cid, secret):
-    client = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    return spotipy.Spotify(client_credentials_manager=client)
+    get_recommendations(a=seeds_artists, b=seeds_tracks, c=seeds_genres)
+    click.echo(seeds_artists)
 
 
 def get_track(spotify, track):
-    print("IMPLEMENT GET TRACK HERE!!!!")
-    return track
+    results = spotify.search(q='track:' + track, type='track')
+    for i in results['tracks']['items']:
+        print(i['name'] + '  ID:' + i['id'])
+    return results
 
 
 def get_artist(spotify, artist):
-    print("IMPLEMENT GET ARTIST HERE!!!!")
-    return artist
+    results = spotify.search(q='artist:' + artist, type='artist')
+    for i in results['artists']['items']:
+        print(i['name'] + '  ID:' + i['id'])
+
+    return results
 
 
-def get_recommendations(spotify, seeds_artists, seeds_tracks, seeds_genres):
-    print("Call get_recommendations with collected seeds here!!!")
-    return ["IMPLEMENT THIS SHIT"]
+def get_recommendations(sp, a, b, c):
+
+  recommendation = sp.recommendations(seed_artists=arid,seed_tracks=trid,
+                            seed_genres=genre)
+
+
+
+cli()
