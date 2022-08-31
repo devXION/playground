032fe9285b4769727059c559427eb662e4c15936
XION <83879915+devXION@users.noreply.github.com>
2022-08-31 11:44:09 +0200

    Final Version of INTCli.py
    Added custom function to ask user if recommended songs should be created in new playlist and added there.

--- INTCli.py
+++ INTCli.py
@@ -30,22 +30,19 @@ def cli(cid, secret):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
                                                    client_secret=secret,
                                                    redirect_uri='http://example.com/callback/',
-                                                   scope='playlist-read-private'))
+                                                   scope='playlist-read-private,playlist-modify-public'))
 
     click.echo("This command will guide you through finding your songs recommendation.")
     seeds_artists = []
     seeds_tracks = []
     seeds_genres = []
 
-    if click.confirm("Would you like to define artist seeds?"):
-        click.echo("""\
-        You can specify an artist in following forms:
-          - A artist name (Metalica)
-          - A artist URL (Example here)
-          - A artist Spotify ID (EXAMPLE HERE)"""
-                   )
+    if click.confirm("Search for an artist?"):
+        click.echo('Search artists name: ')
 
-        artist = click.prompt("Search for an artist (or leave blank to continue)", default="", type=str)
+
+
+        artist = click.prompt("(or leave blank to continue)", default="", type=str)
 
         while artist:
             result = get_artist(sp, artist)
@@ -54,15 +51,10 @@ def cli(cid, secret):
                 artist = click.prompt("Search for an artist", default="", type=str)
                 break
 
-    if click.confirm("Would you like to define track seeds?"):
-        click.echo("""\
-        You can specify an artist in following forms:
-          - A track name (Careless whisper)
-          - A track URL (Example here)
-          - A track Spotify ID (EXAMPLE HERE)"""
-                   )
+    if click.confirm("Would you like to search for a song?"):
+        click.echo('search by name of the song: ')
 
-        track = click.prompt("Search for an track (or leave blank to continue)", default="", type=str)
+        track = click.prompt("(or leave blank to continue)", default="", type=str)
 
         while track:
             result = get_track(sp, track)
@@ -71,7 +63,7 @@ def cli(cid, secret):
                 track = click.prompt("Search for a track", default="", type=str)
                 break
 
-    if click.confirm("Would you like to define genre seeds?"):
+    if click.confirm("Would you like to define genre?"):
         available_genres = sp.recommendation_genre_seeds()["genres"]
         genres_numered = [f"[{i}] {x}" for i, x in enumerate(available_genres)]
         click.echo(f"Available genres:")
@@ -85,28 +77,27 @@ def cli(cid, secret):
             genre = click.prompt("Enter genre # to add:", default="", type=str)
 
     click.echo("DEBUG INFO")
-    click.echo("Seeds artists:")
+    click.echo("Artists ID:")
     click.echo(seeds_artists)
-    click.echo("Seeds tracks:")
+    click.echo("Tracks ID:")
     click.echo(seeds_tracks)
-    click.echo("Seeds genres:")
+    click.echo("Genres defined:")
     click.echo(seeds_genres)
     click.echo()
     get_recommendations(sp, a=seeds_artists, b=seeds_tracks, c=seeds_genres)
-    click.echo(seeds_artists)
 
 
 def get_track(spotify, track):
     results = spotify.search(q='track:' + track, type='track')
     for i in results['tracks']['items']:
-        print(i['name'] + '  ID:' + i['id'])
+        print((i['name']))
     return results
 
 
 def get_artist(spotify, artist):
     results = spotify.search(q='artist:' + artist, type='artist')
     for i in results['artists']['items']:
-        print(i['name'] + '  ID:' + i['id'])
+        print(i['name'])
 
     return results
 
@@ -114,14 +105,26 @@ def get_artist(spotify, artist):
 def get_recommendations(sp, a , b, c):
 
   recommendation = sp.recommendations(seed_artists=a,seed_tracks=b,
-                            seed_genres=c)
+                            seed_genres=c, limit=100)
+
+  songs=[]
+  print('Recommended songs are listed below:')
   for i in recommendation['tracks']:
-      print(i['name'])
+      print(f"[Song]: {i['name']} [Artist]: {i['album']['artists'][0]['name']}")
+      songs.append(i['id'])
+  if click.confirm('Would you like to add these songs to newly created playlist?'):
+      click.prompt('what is your spotify nickname: ')
+      nickname = sp.me()['id']
+      playlist = sp.user_playlist_create(user=nickname, name='RECOMMENDATIONS')['id']
+      sp.playlist_add_items(playlist_id=playlist,items=songs)
 
 
 
 
+     
 
+
+    
 
 
 cli()
